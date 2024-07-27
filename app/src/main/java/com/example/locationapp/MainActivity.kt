cf8f659115cd0b97c130746842481d6ff8892510
git-sajib <samiul.islam.unisel@gmail.com>
2024-07-27 17:20:28 +0600

    RememberLauncherForActivityResult - Starting the Permission request

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.locationapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import android.Manifest
+import androidx.compose.material3.Button
+
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.locationapp.ui.theme.LocationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +30,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LocationAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //codebase
                 }
             }
         }
@@ -31,17 +42,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun LocationDisplay(
+        locationUtils: LocationUtils,
+        context: Context
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocationAppTheme {
-        Greeting("Android")
+        val requestPermissionLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestMultiplePermissions(),
+            onResult = { permissions ->
+                if(permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+                    && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true){
+                    // I have ACCESS to location
+                } else {
+                    // Ask for permission
+                }
+            })
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(text = "Location not available")
+        Button(onClick = {
+            if(locationUtils.hasLocationPermission(context)){
+                // Permission already granted update the location
+            } else {
+                // Request location permission
+                // ...
+            }
+        }) {
+            Text(text = "Get Location")
+        }
+        }
     }
-}
